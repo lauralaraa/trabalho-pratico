199a959fcd7c6a89238198cadea5d60f011a6ff5
Gabriel Santos Martins <1579182@sga.pucminas.br>
2025-06-24 14:03:24 -0300

    Crud do endereço

--- func/endereco.h
+++ func/endereco.h
@@ -3,9 +3,29 @@
 #include<string.h>
 
 typedef struct Endereco {
-    char rua[30];
-    char bairro[30];
-    char cidade[30];
+    char rua[50];
+    char bairro[50];
+    char cidade[50];
     char cep[20];
 }Endereco;
 
+Endereco cadastro_endereco(i) {
+
+     int i,flag;
+    while(flag){
+    Endereco Endereco[i];
+    printf("\nInsira o nome da sua rua:\n");
+    fgets(Endereco[i].rua,50,stdin);
+    printf("\nInsira o nome do seu bairro:\n");
+    fgets(Endereco[i].bairro,50,stdin);
+    printf("\nInsira a cidade em que você mora:\n");
+    fgets(Endereco[i].cidade,50,stdin);
+    printf("\nInsira o CEP\n");
+    fgets(Endereco[i].cep,50,stdin);
+    
+    printf("\nAs informações estão corretas?\nSim=1\nNão=0\n");
+    scanf("%d",&flag);
+    
+    }
+
+}
