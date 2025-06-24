e018e912fcc9a3e589e4fae3564f4d2fcf8c2545
Gabriel Santos Martins <1579182@sga.pucminas.br>
2025-06-24 13:45:40 -0300

    Realocação do endereço

--- func/comprador.h
+++ func/comprador.h
@@ -2,13 +2,6 @@
 #include<stdlib.h>
 #include<string.h>
 
-typedef struct Endereco {
-    char rua[30];
-    char bairro[30];
-    char cidade[30];
-    char cep[20];
-}Endereco;
-
 typedef struct Comprador {
     char nome[100];
     char cpf[15];
--- func/endereco.h
+++ func/endereco.h
@@ -1,3 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
+typedef struct Endereco {
+    char rua[30];
+    char bairro[30];
+    char cidade[30];
+    char cep[20];
+}Endereco;
+
