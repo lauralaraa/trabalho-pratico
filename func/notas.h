74a362d46bf03ac2eb363eee0dfe11483175f718
laura lara <laura.lara.1547870@sga.pucminas.br>
2025-07-01 11:36:48 -0300

    criação do arquivo de notas

--- func/notas.h
+++ func/notas.h
@@ -1,8 +1,21 @@
 #ifndef NOTAS_H
 #define NOTAS_H
+#include<stdio.h>
+#include<stdlib.h>
+#include"compradores.h"
 
 void gerarNota() {
 
+    Comprador comprador;
+
+    FILE *arq = fopen("nota.txt", "w");
+    if (arq != NULL) {
+    fprintf(arq, "Nome: %s\nCPF: %s\nEmail: %s\nEndereço: %s,%s,%s,%s,%s,%s,%s ", comprador.nome, comprador.cpf, comprador.email, comprador.endereco.bairro, comprador.endereco.rua, comprador.endereco.cidade, comprador.endereco.cep);
+    fclose(arq);
+    } else {
+        printf("Erro ao gerar a nota fiscal.\n");
+    }
 }
+
 
 #endif
